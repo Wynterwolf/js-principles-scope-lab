a72317f07b70f7aeeb043dee9e83c4ecd6a64bbf
Samantha Jordan <shinyguns@gmail.com>
2021-03-15 17:13:26 -0500

    done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+//global variable with var
+var customerName = 'bob'
+
+//change to uppercase with function
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+//change global inside the function
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+//change bestCustomer variable
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Karen'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Ted'
+}
